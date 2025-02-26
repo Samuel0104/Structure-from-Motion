2087ac06207b64352e4f25b332d5d6a9ba25ba61
Samuel0104 <samuelmorenovahos@hotmail.com>
2025-02-26 16:35:03 +0000

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,14 +3,11 @@ from features import detect_features, match_features
 from geometry import get_matrices
 from glob import glob
 from pickle import dump
+# from visualization import draw_model
 
 chessboard_images = glob("../images/chess/*.jpg")
 object_images = glob("../images/object/*.jpg")
 num = len(object_images)
-
-K = calibrate_camera(chessboard_images)
-with open("../assets/calibration_matrix.pkl", "wb") as file:
-    dump(K, file)
 
 keypoints = dict()
 for i in range(num):
@@ -26,10 +23,15 @@ for i in range(1, num):
         desc1 = keypoints[f"img{i}"]["descriptor"]
         desc2 = keypoints[f"img{j}"]["descriptor"]
         match = match_features(desc1, desc2)
-        matches[f"{i}_{j}"] = match
+        matches[f"{i}_{j}"] = [(m.distance, m.imgIdx, m.queryIdx, m.trainIdx) for m in match]
+        print(f"{i}_{j}")
 with open("../assets/matches.pkl", "wb") as file:
     dump(matches, file)
 
+K = calibrate_camera(chessboard_images)
+with open("../assets/calibration_matrix.pkl", "wb") as file:
+    dump(K, file)
+    
 matrices = dict()
 for i in range(1, num):
     for j in range(i + 1, num + 1):
@@ -40,5 +42,9 @@ for i in range(1, num):
                                 "Essential": E,
                                 "Rotation": R,
                                 "Translation": t}
+        print(f"{i}_{j}")
 with open("../assets/matrices.pkl", "wb") as file:
     dump(matrices, file)
+    
+# path = "../assets/reconstruction/dense/0/fused.ply"
+# draw_model(path)
