79bbb3fb0ee08d8174b92f4b16697da09fe07c3c
Samuel0104 <samuelmorenovahos@hotmail.com>
2025-02-26 03:20:34 +0000

    Update geometry.py

--- scripts/geometry.py
+++ scripts/geometry.py
@@ -1,20 +1,18 @@
 import cv2 as cv
 import numpy as np
 
-def get_fundamental_matrix():
-    pass
-
-def get_essential_matrix(kp1: tuple[cv.KeyPoint], kp2: tuple[cv.KeyPoint], K: np.ndarray):
+def get_matrices(kp1: tuple[cv.KeyPoint], kp2: tuple[cv.KeyPoint], K: np.ndarray):
     pts1 = np.float32([kp.pt for kp in kp1])
     pts2 = np.float32([kp.pt for kp in kp2])
-    
     min_points = min(pts1.shape[0], pts2.shape[0])
     pts1 = pts1[:min_points]
     pts2 = pts2[:min_points]
-    
-    E, _ = cv.findEssentialMat(pts1, pts2, K, cv.RANSAC)
+
+    F, _ = cv.findFundamentalMat(pts1, pts2, cv.RANSAC)
+    E = K.T @ F @ K
+    # E, _ = cv.findEssentialMat(pts1, pts2, K, cv.RANSAC)
     _, R, t, _ = cv.recoverPose(E, pts1, pts2, K)
-    return R, t
+    return F, E, R, t
 
 if __name__ == "__main__":
     from calibration import calibrate_camera
@@ -26,6 +24,8 @@ if __name__ == "__main__":
     object_images = glob("../images/object/*.jpg")
     kp1, desc1 = detect_features(object_images[0])
     kp2, desc2 = detect_features(object_images[1])
-    R, t = get_essential_matrix(kp1, kp2, K)
-    print("Rotation matrix:\n", R)
-    print("Translation vector:\n", t)
+    F, E, R, t = get_matrices(kp1, kp2, K)
+    print("Fundamental matrix:\n", F)
+    print("\nEssential matrix:\n", E)
+    print("\nRotation matrix:\n", R)
+    print("\nTranslation vector:\n", t)
